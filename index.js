815d474598eabd78f5040792efe6631bdd3760ae
saher saleem <saleemsaba281@outlook.com>
2024-03-11 20:09:22 +0500

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import inquirer from "inquirer";
-let answers = await inquirer.prompt({
-    type: "input",
-    name: "sentence",
-    message: "Enter your sentence please:"
-});
-const a = answers.sentence.trim().split(" ");
-console.log(`Your sentence word count is : ${a.length}`);
+async function askQuestion() {
+    let answers = await inquirer.prompt({
+        type: "input",
+        name: "sentence",
+        message: "Enter your sentence please:"
+    });
+    const a = answers.sentence.trim().split(" ");
+    console.log(`Your sentence word count is : ${a.length}`);
+}
+do {
+    await askQuestion();
+    var res = await inquirer.prompt({
+        type: "input",
+        name: "restart",
+        message: "Do you want to start again . Press Y"
+    });
+} while (res.restart == "y" || res.restart == "yes" || res.restart == "YES" || res.restart == "Y");
